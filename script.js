b0944689ff2c95b854a11bd402387b867e2e92fe
nope-nop <slampisehoah@gmail.com>
2024-01-31 08:26:37 -0500

    calculator design complete

---
+++ script.js
@@ -0,0 +1,15 @@
+function operation(num1, num2, input){
+    if (input == "+"){
+        return num1 + num2;
+    } else if (input == "-"){
+        return num1 - num2;
+    } else if (input == "*"){
+        return num * num2;
+    } else if (input == "/"){
+        return num1 / num2;
+    } else return 0;
+}
+
+const screen = document.getElementById("");
+
+console.log('hello world');
